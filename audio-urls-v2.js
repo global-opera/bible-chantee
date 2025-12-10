99eaeb51dd87527e84fd132edf80a5e136db0129
opera <scassani@gmail.com>
2025-12-10 16:00:44 +0100

    audio-urls-v2.js - Exode 40 chapitres complet (Google Drive)

--- audio-urls-v2.js
+++ audio-urls-v2.js
@@ -1,8 +1,54 @@
-// audio-urls-v2.js - URLs MP3 V2 pour Bible Chantee
-// Source: GitHub audio-v2/
+// Bible Chantee - URLs MP3 Version 2
+// Source: Google Drive (dossier public)
+// Format URL: https://drive.google.com/uc?export=download&id=FILE_ID
+
 const audioUrlsV2 = {
+    // Genese - 50 chapitres (a completer)
     "01": {
         1: "https://raw.githubusercontent.com/global-opera/bible-chantee/main/audio-v2/01_GEN_01.mp3"
+    },
+    
+    // Exode - 40 chapitres (COMPLET)
+    "02": {
+        1: "https://drive.google.com/uc?export=download&id=1yiqnuZkHHPCVY9R8anmu4KmH5zhW-Cw6",
+        2: "https://drive.google.com/uc?export=download&id=14A5YG_mARA5Be9zHnvoUWq0WXQ8tlX15",
+        3: "https://drive.google.com/uc?export=download&id=1D6BRiugY_Cp5LHvQytiERHMqaOdakW3k",
+        4: "https://drive.google.com/uc?export=download&id=1KdezXC3XmNHIzddfp5K3FOs_TzAo1uSc",
+        5: "https://drive.google.com/uc?export=download&id=1m82CZdB0DrhFIB04nmSWcK-fGfMik5YC",
+        6: "https://drive.google.com/uc?export=download&id=1QnXbgICNZKyJk3DJlo-h84Gw99R862-A",
+        7: "https://drive.google.com/uc?export=download&id=1R3mMDaL9NdGBmHY0sLzY2emIfsRoeOxC",
+        8: "https://drive.google.com/uc?export=download&id=11RHFQ7hJkfTBNi2UEFsM8KwUc-P8LCwG",
+        9: "https://drive.google.com/uc?export=download&id=1tRH6fnYCKvHbZ85F7w72HdSLIqcKYGGX",
+        10: "https://drive.google.com/uc?export=download&id=1uQ_XyYfmBS_30GSwyfTADmH9F-GUIb5N",
+        11: "https://drive.google.com/uc?export=download&id=1Cwwlpoz4FYx6BgXsztzRdS7cEW6mdqNR",
+        12: "https://drive.google.com/uc?export=download&id=1EkNRK3wo1bfedg0QlBo7hcpa-hIbz-ob",
+        13: "https://drive.google.com/uc?export=download&id=13-6Xvi1LgKdBscjwgqWZHXMKq2msJ5h6",
+        14: "https://drive.google.com/uc?export=download&id=1MpbLqPStlhwnawS7g3gmQZ_SgkdM8ks6",
+        15: "https://drive.google.com/uc?export=download&id=1_hFIhf3tL2IKuPOZ-ECbmrJXPSZvcVVQ",
+        16: "https://drive.google.com/uc?export=download&id=1dVWvkYUQHvGtpcQpf3AJNdhrKzByJku6",
+        17: "https://drive.google.com/uc?export=download&id=1h4bq3YGaWuWvxnzChj5RyNLMEF1pCWbF",
+        18: "https://drive.google.com/uc?export=download&id=1WwHiiSt_rbC-PlPQKeZPS-Li-3dFqk1K",
+        19: "https://drive.google.com/uc?export=download&id=10He-8587Yw1cWDyFzMsakOPeEUNGNj25",
+        20: "https://drive.google.com/uc?export=download&id=1jcjd0LqUj9PeYx08GeLSOjIbHrFuLfZi",
+        21: "https://drive.google.com/uc?export=download&id=1uunmPU_bhgC0OKqbJzcbC7q2MBNdMkIo",
+        22: "https://drive.google.com/uc?export=download&id=1yT6UyksXpMjYV_XkxmQibnoZS3uNtxUL",
+        23: "https://drive.google.com/uc?export=download&id=1zSLJqJtXla8XDqZbsASqFaW-OA_0tdr8",
+        24: "https://drive.google.com/uc?export=download&id=15xZLE9VhYiwBD2e9h-xCYG_ySEHMBorX",
+        25: "https://drive.google.com/uc?export=download&id=14Yv8Hv93uwJvOh9ciNJNSyMumiONBFwv",
+        26: "https://drive.google.com/uc?export=download&id=1R5awb3e3Cl2dn2VN0j71tI7b4r8vdzVJ",
+        27: "https://drive.google.com/uc?export=download&id=1LA4wEo6hoKy5wBJvECCHCGRvRSislPrb",
+        28: "https://drive.google.com/uc?export=download&id=1OW-f0HXMkIS2U63ct96pfPrcYr_XhtU7",
+        29: "https://drive.google.com/uc?export=download&id=101795-8ZWllaxJgqoBoLD-7YnCEZYh6m",
+        30: "https://drive.google.com/uc?export=download&id=1dyr5quBxVaAgL2vJZ_OeRO59LQwnBFIk",
+        31: "https://drive.google.com/uc?export=download&id=1gGWNeq8vxe6znNT2pnSr21FCiGXKYLXT",
+        32: "https://drive.google.com/uc?export=download&id=1nH9x5B-FA3lJRd8Z-3i8GoK23NWJMS7p",
+        33: "https://drive.google.com/uc?export=download&id=1qXxN8-YuVUafWNfSehtJfF3hQh5uvq9V",
+        34: "https://drive.google.com/uc?export=download&id=1myRVWysg9uoN6nZs-Rf7HXLWbC5WwoGe",
+        35: "https://drive.google.com/uc?export=download&id=1ppNddLNAyHmxEq8_QdcKKn00jpnKAdhL",
+        36: "https://drive.google.com/uc?export=download&id=1R2_914cVnDpr0pjaOpXqNWseslU344O5",
+        37: "https://drive.google.com/uc?export=download&id=1NFN_QC-91u8ufYKYH3n9-RadPptqAVRS",
+        38: "https://drive.google.com/uc?export=download&id=18paUbA7O1dRHqe---51mkqDp9M9Bv6VF",
+        39: "https://drive.google.com/uc?export=download&id=18_atfN_XSHCRbJj_D5bprkrp-JuVH-H_",
+        40: "https://drive.google.com/uc?export=download&id=1lITJgbahCgyCagarJ9jj2TW0isnWJVmB"
     }
-    // Autres chapitres a ajouter au fur et a mesure
 };
