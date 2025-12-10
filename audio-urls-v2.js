26f7141ca4092ee3d5f3949a08f39cbb6e9f67ae
opera <scassani@gmail.com>
2025-12-10 13:36:38 +0100

    audio-urls-v2.js - format compatible avec lecteur

--- audio-urls-v2.js
+++ audio-urls-v2.js
@@ -1,16 +1,8 @@
-// Bible Chantee V2 - URLs des fichiers MP3
+// audio-urls-v2.js - URLs MP3 V2 pour Bible Chantee
 // Source: GitHub audio-v2/
-
 const audioUrlsV2 = {
-    // Format: "BOOK_CHAPTER": "URL_MP3"
-    
-    // 01_GEN - Genese
-    "01_GEN_01": "https://raw.githubusercontent.com/global-opera/bible-chantee/main/audio-v2/01_GEN_01.mp3"
-    
+    "01": {
+        1: "https://raw.githubusercontent.com/global-opera/bible-chantee/main/audio-v2/01_GEN_01.mp3"
+    }
     // Autres chapitres a ajouter au fur et a mesure
 };
-
-// Export pour utilisation dans le lecteur
-if (typeof module !== 'undefined') {
-    module.exports = audioUrlsV2;
-}
