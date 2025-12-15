6cf76e67bd6f77aca4f0ac943fb3d52e73e6b1e7
Stephane Cassani <info@manialibris.com>
2025-12-15 19:49:04 +0100

    Promessas/Confessions + multilang shared (PT)

---
+++ promessas-data.js
@@ -0,0 +1,21 @@
+const PROMESSAS = {
+  PT: {
+    ansiedade: {
+      titulo: "Ansiedade e Medo",
+      sub: "Encontre paz interior",
+      salmos: [23, 27, 46, 91]
+    },
+    paz: {
+      titulo: "Paz e Descanso",
+      sub: "Entre no descanso de Deus",
+      salmos: [4, 23, 37, 131]
+    }
+  },
+  FR: {
+    anxiete: {
+      titulo: "Anxiété et Peur",
+      sub: "Trouver la paix intérieure",
+      salmos: [23, 27, 46, 91]
+    }
+  }
+};
