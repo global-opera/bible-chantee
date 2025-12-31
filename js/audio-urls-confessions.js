c70d6e7d404dcf12c1cc5638199fd87212c3be51
Stephane Cassani <info@manialibris.com>
2025-12-31 19:18:13 +0100

    Confessions: lecteur avec audio R2 + lyrics FR/PT

---
+++ js/audio-urls-confessions.js
@@ -0,0 +1,29 @@
+// Audio URLs - Confessions (Cloudflare R2)
+const R2_BASE = "https://pub-2dc4dfed0c5e45338913878f35d4d56a.r2.dev/confessions";
+
+window.CONFESSIONS_AUDIO = {
+  FR: {
+    "01": R2_BASE + "/FR/01_Joie.mp3",
+    "02": R2_BASE + "/FR/02_Gueri.mp3",
+    "03": R2_BASE + "/FR/03_Sante.mp3",
+    "04": R2_BASE + "/FR/04_Vainqueur.mp3",
+    "05": R2_BASE + "/FR/05_Prospere.mp3",
+    "06": R2_BASE + "/FR/06_Reconnaissant.mp3",
+    "07": R2_BASE + "/FR/07_Heureux.mp3",
+    "08": R2_BASE + "/FR/08_Beni.mp3",
+    "09": R2_BASE + "/FR/09_Amen.mp3",
+    "10": R2_BASE + "/FR/10_Shalom.mp3"
+  },
+  PT: {
+    "01": R2_BASE + "/PT/01_Alegria.mp3",
+    "02": R2_BASE + "/PT/02_Curado.mp3",
+    "03": R2_BASE + "/PT/03_Saude.mp3",
+    "04": R2_BASE + "/PT/04_Vencedor.mp3",
+    "05": R2_BASE + "/PT/05_Prospero.mp3",
+    "06": R2_BASE + "/PT/06_Grato.mp3",
+    "07": R2_BASE + "/PT/07_Feliz.mp3",
+    "08": R2_BASE + "/PT/08_Abencoado.mp3",
+    "09": R2_BASE + "/PT/09_Amem.mp3",
+    "10": R2_BASE + "/PT/10_Shalom.mp3"
+  }
+};
