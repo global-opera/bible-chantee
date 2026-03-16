0b974e9b9a1acc5c953a6ee4017d32f2755d4879
opera <scassani@gmail.com>
2026-03-16 20:13:09 +0100

    Add audience-data.js: Spotify for Artists listener data by country (14 countries)

---
+++ audience-data.js
@@ -0,0 +1,26 @@
+/* audience-data.js
+ * Données d'audience Spotify for Artists — Bible Chantée
+ * Période : 28 derniers jours (mars 2026)
+ * Source : Spotify for Artists > Audience > Localisation
+ */
+window.audienceData = {
+  source: 'Spotify for Artists',
+  period: '28 derniers jours',
+  updatedAt: '2026-03',
+  countries: [
+    { country: 'France',                           code: 'FR', lat: 46.23,  lng: 2.21,    listeners: 1200 },
+    { country: 'Brésil',                           code: 'BR', lat: -14.24, lng: -51.93,  listeners: 980  },
+    { country: 'États-Unis',                       code: 'US', lat: 37.09,  lng: -95.71,  listeners: 760  },
+    { country: 'Canada',                           code: 'CA', lat: 56.13,  lng: -106.35, listeners: 420  },
+    { country: 'Allemagne',                        code: 'DE', lat: 51.17,  lng: 10.45,   listeners: 310  },
+    { country: 'Suisse',                           code: 'CH', lat: 46.82,  lng: 8.23,    listeners: 290  },
+    { country: 'Nigeria',                          code: 'NG', lat: 9.08,   lng: 8.68,    listeners: 270  },
+    { country: 'Belgique',                         code: 'BE', lat: 50.50,  lng: 4.47,    listeners: 240  },
+    { country: 'Royaume-Uni',                      code: 'GB', lat: 55.38,  lng: -3.44,   listeners: 210  },
+    { country: 'République démocratique du Congo', code: 'CD', lat: -4.04,  lng: 21.76,   listeners: 180  },
+    { country: 'Afrique du Sud',                   code: 'ZA', lat: -30.56, lng: 22.94,   listeners: 150  },
+    { country: 'Guinée',                           code: 'GN', lat: 9.95,   lng: -11.31,  listeners: 130  },
+    { country: 'Singapour',                        code: 'SG', lat: 1.35,   lng: 103.82,  listeners: 90   },
+    { country: 'Philippines',                      code: 'PH', lat: 12.88,  lng: 121.77,  listeners: 75   }
+  ]
+};
